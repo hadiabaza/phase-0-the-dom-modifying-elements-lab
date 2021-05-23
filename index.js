fd4cac3e49344f868e46d735bfd7fab1ea24b76a
Hadi Abaza <Hadiabaza1@gmail.com>
2021-05-23 19:24:21 -0400

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Hadi Abaza is the champion";
+document.body.append(newHeader);
+
+const firstDiv = document.createELement("div");
+document.body.append(firstDiv);
